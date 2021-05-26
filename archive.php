c6a5c5225bc2ae6cfc0816fa6eca98b85b7152c3
Lewis <l.birch@lancaster.ac.uk>
2021-05-26 17:28:51 +0100

    Added webpack and dependencies, as well as changed functions.php enqueue locations.

--- archive.php
+++ archive.php
@@ -2,7 +2,7 @@
 
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri(
-    "/images/ocean.jpg"
+    '/images/ocean.jpg'
   ); ?>);"></div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
@@ -21,8 +21,8 @@
 
         <div class="metabox">
           <p>Posted by <?php the_author_posts_link(); ?> on 
-          <?php the_time("n.j.y"); ?> in 
-          <?php echo get_the_category_list(", "); ?>
+          <?php the_time('n.j.y'); ?> in 
+          <?php echo get_the_category_list(', '); ?>
           </p>
         </div>
 
