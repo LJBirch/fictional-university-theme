02daa1e1d366efa259954d228586bf5ea4d83777
Lewis <l.birch@lancaster.ac.uk>
2021-05-28 18:01:42 +0100

    Added single campus post type page.

--- archive-campus.php
+++ archive-campus.php
@@ -8,22 +8,19 @@ pageBanner([
 
 <div class="container container--narrow page-section">
     <div class="acf-map">
-    <?php
-    while (have_posts()) {
+    <?php while (have_posts()) {
 
       the_post();
       $mapLocation = get_field('map_location');
-      print_r($mapLocation);
       ?>
       <div class="marker" data-lat="<?php echo $mapLocation[
         'lat'
       ]; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <?php echo $mapLocation['address']; ?>
       </div>
     <?php
-    }
-
-    echo paginate_links();
-    ?>
+    } ?>
     </div>
 </div>
 
