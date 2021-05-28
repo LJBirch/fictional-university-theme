8ab0c57d65d1b729a29130d06b028e6127f215b5
Lewis <l.birch@lancaster.ac.uk>
2021-05-28 13:22:10 +0100

    Created function to display dynamic page banner.

--- page.php
+++ page.php
@@ -3,20 +3,10 @@
 get_header();
 
 while (have_posts()) {
-  the_post(); ?>
-    
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri(
-      "/images/ocean.jpg"
-    ); ?>);">
-    </div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>  
-  </div>
+
+  the_post();
+  pageBanner();
+  ?>
 
   <div class="container container--narrow page-section">
     
@@ -39,7 +29,7 @@ while (have_posts()) {
 
     <?php
     $testArray = get_pages([
-      "child_of" => get_the_ID(),
+      'child_of' => get_the_ID(),
     ]);
 
     if ($theParent or $testArray) { ?>
@@ -56,9 +46,9 @@ while (have_posts()) {
         }
 
         wp_list_pages([
-          "title_li" => null,
-          "child_of" => $findChildrenOf,
-          "sort_column" => "menu_order",
+          'title_li' => null,
+          'child_of' => $findChildrenOf,
+          'sort_column' => 'menu_order',
         ]);
         ?>
       </ul>
